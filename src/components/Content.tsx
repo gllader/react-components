83b5c9def10566f3ab9330d00383cc56de2471c2
gll <guilherme.lopes123@hotmail.com>
2021-05-21 21:20:05 -0300

    create components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,34 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+    }>;
+  Runtime: string;
+}
+
+interface contentProps {
+  movies: MovieProps[]
+}
+
+export function Content({movies}: contentProps) {
+  return(
+    <main>
+    <div className="movies-list">
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.imdbID}
+          title={movie.Title}
+          poster={movie.Poster}
+          runtime={movie.Runtime}
+          rating={movie.Ratings[0].Value}
+        />
+      ))}
+    </div>
+  </main>
+  );
 }
